616fe5b41932325bb4dc0d5f6b8d6679d86c411f
LucasCalcada <lucasmgmcalcada@gmail.com>
2026-03-14 10:06:24 -0300

    chore: init core-service workspace

---
+++ back/core-service/src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import config from "./config";
+
+const app = express();
+app.use(express.json());
+
+app.listen(config.port, () => {
+  console.log(`Core-service started on port ${config.port}`);
+});
