e1cda11f24b6336a398451ca188ceadcd2d48643
Lucas Calçada <lucasmgmcalcada@gmail.com>
2026-03-22 15:32:39 -0300

    feat(front): sidebar styling

--- front/src/components/sidebar/index.tsx
+++ front/src/components/sidebar/index.tsx
@@ -4,7 +4,7 @@ import Options from "./options";
 
 function SideBar() {
   return (
-    <div className="flex flex-col left-0 absolute h-screen bg-neutral-900 p-2">
+    <div className="flex flex-col left-0 absolute h-screen bg-neutral-900 p-2 border-neutral-800 border-r-1">
       <Header />
       <SideNav />
       <Options />
