a0a5d7335d03e9e6243542a902445d19e751ca65
LucasCalcada <lucasmgmcalcada@gmail.com>
2026-04-03 17:26:25 -0300

    feat(core-service): add logger

--- back/core-service/src/helpers/routeRegistry.ts
+++ back/core-service/src/helpers/routeRegistry.ts
@@ -1,5 +1,6 @@
 import { Request, type Express } from 'express';
 import wrapHandler from '../middlewares/routeWrapper';
+import log from './logger';
 
 export type RouteHandler = (req: Request) => Promise<any>;
 
@@ -19,6 +20,6 @@ export function setupRouter(app: Express) {
   routes.forEach((r) => {
     const wrappedHandler = wrapHandler(r.handler);
     app[r.method](r.path, wrappedHandler);
-    console.log(`Registered route ${r.method} ${r.path}`);
+    log('SETUP', `Registered route ${r.method} ${r.path}`);
   });
 }
--- back/core-service/src/index.ts
+++ back/core-service/src/index.ts
@@ -3,12 +3,15 @@ import config from '@app/config';
 import { setupRouter } from '@helpers/routeRegistry';
 import errorMiddleware from './middlewares/error';
 import './api';
+import log from './helpers/logger';
+import loggerMiddleware from './middlewares/logger';
 
 const app = express();
 app.use(express.json());
+app.use(loggerMiddleware);
 setupRouter(app);
 app.use(errorMiddleware);
 
 app.listen(config.port, () => {
-  console.log(`Core-service started on port ${config.port}`);
+  log('SETUP', `Core-service started on port ${config.port}`);
 });
