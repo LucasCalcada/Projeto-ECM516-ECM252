22448fcde32cb206ea922d5ee1b7a97497132d19
LucasCalcada <lucasmgmcalcada@gmail.com>
2026-04-03 12:45:56 -0300

    feat(core-service): store reference to building on user table

--- back/core-service/src/db/schema/user.ts
+++ back/core-service/src/db/schema/user.ts
@@ -1,10 +1,12 @@
 import { InferInsertModel, InferSelectModel } from 'drizzle-orm';
 import { pgTable, text, uuid, boolean } from 'drizzle-orm/pg-core';
 import residency from './residency';
+import building from './building';
 
 export const user = pgTable('users', {
   id: uuid('id').primaryKey().notNull().defaultRandom(),
   residencyId: uuid('residencyId').references(() => residency.id),
+  buildingId: uuid('buildingId').references(() => building.id),
   name: text('name'),
   permissions: text('permissions').array(),
   active: boolean('active'),
