a60f19e62d6972e369825be9e0d07d09b0ac03bf
LucasCalcada <lucasmgmcalcada@gmail.com>
2026-04-12 19:07:13 -0300

    chore(auth-service): format index.ts file

--- back/auth-service/src/index.ts
+++ back/auth-service/src/index.ts
@@ -19,12 +19,11 @@ interface User {
 }
 const users: User[] = [];
 
-
 app.get('/users', (req, res) => {
   res.json(users);
 });
 
-app.post('/users', async(req, res) => {
+app.post('/users', async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     const user: User = { username: req.body.username, password: hashedPassword };
@@ -35,33 +34,38 @@ app.post('/users', async(req, res) => {
   }
 });
 
-app.post('/login', async(req, res) => {
-    const user = users.find(user => user.username === req.body.username);
-    if (user == null) {
-        return res.status(400).send('Credenciais inválidas');
+app.post('/login', async (req, res) => {
+  const user = users.find((user) => user.username === req.body.username);
+  if (user == null) {
+    return res.status(400).send('Credenciais inválidas');
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      const token = jwt.sign(
+        { username: user.username },
+        process.env.JWT_ACCESS_TOKEN_SECRET as string,
+        { expiresIn: '1h' },
+      );
+      res.status(200).json({ accessToken: token });
+    } else {
+      res.status(400).send('Credenciais inválidas');
     }
-    try {        
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            const token = jwt.sign({ username: user.username }, process.env.JWT_ACCESS_TOKEN_SECRET as string, { expiresIn: '1h' });
-            res.status(200).json({ accessToken: token });
-        } else {
-            res.status(400).send('Credenciais inválidas');
-        }
-    } catch {
-        res.status(500).send();
-    }
+  } catch {
+    res.status(500).send();
+  }
 });
 app.get('/protected', authToken, (req, res) => {
-    res.json({ message: 'Acesso permitido' });
+  res.json({ message: 'Acesso permitido' });
 });
 
 function authToken(req: express.Request, res: express.Response, next: express.NextFunction) {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.status(401).send('Acesso negado');
-    jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET as string, (err, user) => {
-        if (err) return res.status(403).send('Acesso negado');
-        next();
-    });
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.status(401).send('Acesso negado');
+  jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET as string, (err, user) => {
+    if (err) return res.status(403).send('Acesso negado');
+    next();
+  });
 }
 app.listen(3000);
+
