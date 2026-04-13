c9a3ce2e57630fe48c0bc58f8b6e927026e88895
LucasCalcada <lucasmgmcalcada@gmail.com>
2026-04-12 21:48:11 -0300

    feat(auth-service): refactor login route

--- back/auth-service/src/routes/index.ts
+++ back/auth-service/src/routes/index.ts
@@ -1,5 +1,7 @@
 import { registerRoute } from '@app/helpers/routeRegistry';
 
 import createUserRoute from './account/create/route';
+import authenticateUserRoute from './account/login/route';
 
 registerRoute(createUserRoute);
+registerRoute(authenticateUserRoute);
