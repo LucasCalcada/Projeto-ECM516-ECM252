9ded7c8ae4f1f3dbfe8d036cfc5e8b66c7e4b48c
LucasCalcada <lucasmgmcalcada@gmail.com>
2026-04-12 21:47:30 -0300

    feat(auth-service): hash password

--- back/auth-service/src/routes/account/create/handler.ts
+++ back/auth-service/src/routes/account/create/handler.ts
@@ -1,11 +1,12 @@
 import client from '@app/db/client';
 import { accounts } from '@app/db/schema/account';
+import hashPassword from '@app/helpers/hashPassword';
 import BadRequest from '@app/middlewares/error/errors/BadRequest';
 import { Request } from 'express';
 
 export default async function createUser(req: Request) {
-  const { name, hashedPassword, phone, email } = req.body;
-  const isMissingVar = [name, hashedPassword, phone, email].some(
+  const { name, password, phone, email } = req.body;
+  const isMissingVar = [name, password, phone, email].some(
     (v) => v === undefined || v === '' || v === null,
   );
 
@@ -15,6 +16,8 @@ export default async function createUser(req: Request) {
 
   // TODO: validate email/phone
   // TODO: check for unique email/phone
+
+  const hashedPassword = await hashPassword(password);
 
   const [createdUser] = await client
     .insert(accounts)
