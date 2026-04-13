355d2e77ae83e75e10cd7b9e95ed74704ec431c9
LucasCalcada <lucasmgmcalcada@gmail.com>
2026-04-12 21:48:51 -0300

    feat(auth-service): use refactored routes

--- back/auth-service/src/index.ts
+++ back/auth-service/src/index.ts
@@ -1,66 +1,17 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
-import cors from 'cors';
-import jwt from 'jsonwebtoken';
-import { fileURLToPath } from 'url';
 import config from '@app/config';
+import { setupRouter } from '@helpers/routeRegistry';
+import errorMiddleware from './middlewares/error';
+import './routes';
+import log from './helpers/logger';
+import loggerMiddleware from './middlewares/logger';
 
 const app = express();
-app.use(cors());
 app.use(express.json());
-interface User {
-  username: string;
-  password: string;
-}
-const users: User[] = [];
+app.use(loggerMiddleware);
+setupRouter(app);
+app.use(errorMiddleware);
 
-app.get('/users', (req, res) => {
-  res.json(users);
+app.listen(config.port, () => {
+  log('SETUP', `Core-service started on port ${config.port}`);
 });
-
-app.post('/users', async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const user: User = { username: req.body.username, password: hashedPassword };
-    users.push(user);
-    res.status(201).send();
-  } catch {
-    res.status(500).send();
-  }
-});
-
-app.post('/login', async (req, res) => {
-  const user = users.find((user) => user.username === req.body.username);
-  if (user == null) {
-    return res.status(400).send('Credenciais inválidas');
-  }
-  try {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      const token = jwt.sign(
-        { username: user.username },
-        config.jwtToken,
-        { expiresIn: '1h' },
-      );
-      res.status(200).json({ accessToken: token });
-    } else {
-      res.status(400).send('Credenciais inválidas');
-    }
-  } catch {
-    res.status(500).send();
-  }
-});
-app.get('/protected', authToken, (req, res) => {
-  res.json({ message: 'Acesso permitido' });
-});
-
-function authToken(req: express.Request, res: express.Response, next: express.NextFunction) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (token == null) return res.status(401).send('Acesso negado');
-  jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET as string, (err, user) => {
-    if (err) return res.status(403).send('Acesso negado');
-    next();
-  });
-}
-app.listen(3000);
-
