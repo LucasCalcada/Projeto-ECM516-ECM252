92384328908336688e67be4313f3c486730a1b30
Alan Martins Leandro <alan.leandro@ost.com.br>
2026-04-28 11:14:31 -0300

    chore(root): add flat eslint config

---
+++ eslint.config.cjs
@@ -0,0 +1,35 @@
+const js = require('@eslint/js');
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const prettierPlugin = require('eslint-plugin-prettier');
+const prettierConfig = require('eslint-config-prettier');
+const globals = require('globals');
+
+module.exports = [
+  {
+    ignores: ['**/node_modules/**', '**/dist/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...prettierConfig.rules,
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'prettier/prettier': ['error', { singleQuote: true, semi: true }],
+    },
+  },
+];
