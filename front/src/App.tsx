21694c53ccda4f14e4930209fa2857242f36875d
Vinicius-Takashi <vnakatsui2003@gmail.com>
2026-05-29 00:51:50 -0300

    /feat: Routes protection

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
+import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router';
 import routes from './routes';
 import SidebarLayout from './layouts/sidebarLayout';
 import type { RouteConfig } from './routes/route';
@@ -8,18 +8,45 @@ function CreateRouteEntry(r: RouteConfig) {
   return <Route path={r.path} element={<r.viewComponent />} key={r.path} />;
 }
 
+function RequireAccountAuth() {
+  if (!localStorage.getItem('accountToken')) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+}
+
+function RequireUserAuth() {
+  if (!localStorage.getItem('accountToken')) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (!localStorage.getItem('userToken')) {
+    return <Navigate to="/buildingSelector" replace />;
+  }
+
+  return <Outlet />;
+}
+
 function App() {
   const sidebarViews = routes.filter((r) => r.layout == 'sidebar').map(CreateRouteEntry);
 
-  const defaultViews = routes.filter((r) => r.layout == 'none').map(CreateRouteEntry);
+  const publicViews = routes
+    .filter((r) => r.layout == 'none' && r.path !== '/buildingSelector')
+    .map(CreateRouteEntry);
+
+  const accountViews = routes.filter((r) => r.path === '/buildingSelector').map(CreateRouteEntry);
 
   return (
     <ToastProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
-          <Route>{defaultViews}</Route>
-          <Route element={<SidebarLayout />}>{sidebarViews}</Route>
+          <Route>{publicViews}</Route>
+          <Route element={<RequireAccountAuth />}>{accountViews}</Route>
+          <Route element={<RequireUserAuth />}>
+            <Route element={<SidebarLayout />}>{sidebarViews}</Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </ToastProvider>
