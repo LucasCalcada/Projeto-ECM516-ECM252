4a532d54b1142a45188631e7d3c6a638575c9172
LucasCalcada <lucasmgmcalcada@gmail.com>
2026-06-05 22:24:27 -0300

    chore(reservation-service): rename permissions

--- back/reservation-service/src/helpers/permissions.ts
+++ back/reservation-service/src/helpers/permissions.ts
@@ -1,9 +1,9 @@
 import Unauthorized from '@app/middlewares/error/errors/Unauthorized';
 import { Context } from '@app/middlewares/routeWrapper';
 
-export const CREATE_RESERVATION_PERMISSION = '@Reservation:Create';
-export const VIEW_BUILDING_RESERVATION_PERMISSION = '@Reservation:ViewBuilding';
-export const VIEW_RESIDENCY_RESERVATION_PERMISSION = '@Reservation:ViewResidency';
+export const CREATE_RESERVATION_PERMISSION = '@reservation:create';
+export const VIEW_BUILDING_RESERVATION_PERMISSION = '@reservation:view:building';
+export const VIEW_RESIDENCY_RESERVATION_PERMISSION = '@reservation:view:residency';
 
 function hasPermission(ctx: Context, permission: string) {
   return ctx.auth.permissions.includes(permission);
